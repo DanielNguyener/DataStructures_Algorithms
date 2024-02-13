7193c303beb6eface9a8db85fc30e9dfff2ccee4
riceweener <danielnguyen7574@gmail.com>
2024-02-13 10:15:29 -0600

    finished PA2 (deque)

--- PA2/starter/main.cpp
+++ PA2/starter/main.cpp
@@ -2,6 +2,24 @@
 #include "deque.h"
 using namespace std;
 
+void testDequeDeconstructor(){
+
+        Deque<int> q;
+        q.insertFirst(3);
+        q.insertFirst(2);
+        q.insertFirst(1);
+
+        cout << "size before deconstruction: " << q.size() << endl;
+
+        q.~Deque();
+
+        if(q.size() != 0){
+            cout << "destructor failed, size: " << q.size() << endl;
+        }else{
+            cout << "destructor passed" << endl;
+        }
+    }
+
 int main() {
     // Test insertFirst
     {
@@ -24,7 +42,7 @@ int main() {
         
     }
     
-    // Test insertLast
+    // // Test insertLast
     {
         Deque<int> q;
         q.insertLast(1);
@@ -42,6 +60,106 @@ int main() {
         }
         
     }
+
+    //test copy constructor
+
+    {
+        Deque<int> q;
+        q.insertFirst(3);
+        q.insertFirst(2);
+        q.insertFirst(1);
+
+        Deque<int> copyConstructor(q);
+
+        if (copyConstructor.size() != 3) {
+            cout << "Test copyConstructor failed: Incorrect size: " << copyConstructor.size() << endl;
+            
+        } else if (copyConstructor.first() != 1 || copyConstructor.last() != 3) {
+            cout << "Test copyConstructor failed: Incorrect elements" << endl;
+   
+        } else {
+            cout << "Test copyConstructor passed" << endl;
+        }
+
+    }
+
+    // test copy assignment
+
+    {
+        Deque<int> q;
+        q.insertFirst(3);
+        q.insertFirst(2);
+        q.insertFirst(1);
+
+        Deque<int> copyConstructor;
+
+        copyConstructor = q;
+
+        if (copyConstructor.size() != 3) {
+            cout << "Test copyConstructor failed: Incorrect size: " << copyConstructor.size() << endl;
+            
+        } else if (copyConstructor.first() != 1 || copyConstructor.last() != 3) {
+            cout << "Test copyConstructor failed: Incorrect elements" << endl;
+   
+        } else {
+            cout << "Test copyConstructor passed" << endl;
+
+        } 
+    }
+
+    // test remove
+    {
+        Deque<int> q;
+        q.insertFirst(3);
+        q.insertFirst(2);
+        q.insertFirst(1);
+
+        int score = 0;
+
+
+        while(!q.isEmpty()){
+            if(q.last() != q.removeLast()){
+                cout << "data doesn't match" << endl;
+            }else{
+                score++;
+            }
+        }
+
+        if(score == 3){
+            cout << "removeLast works" << endl;
+        }else{
+            cout << "remove incorrect number elements" << endl;
+        }
+    }
+
+    //test removefirst
+
+    {
+        Deque<int> q;
+        q.insertFirst(3);
+        q.insertFirst(2);
+        q.insertFirst(1);
+
+        int score = 0;
+
+
+        while(!q.isEmpty()){
+            if(q.first() != q.removeFirst()){
+                cout << "data doesn't match" << endl;
+            }else{
+                score++;
+            }
+        }
+
+        if(score == 3){
+            cout << "removeFirst works" << endl;
+        }else{
+            cout << "remove incorrect number elements" << endl;
+        }
+
+    }
+
+    testDequeDeconstructor();
 
     return 0;
 }
