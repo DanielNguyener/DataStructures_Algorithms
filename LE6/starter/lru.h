8dcd1e77bfef0212a6e8992b4014aa071978f039
Daniel <danielnguyen7574@gmail.com>
2024-03-26 18:58:59 -0500

    Finished LE6

--- LE6/starter/lru.h
+++ LE6/starter/lru.h
@@ -45,18 +45,90 @@ LRUCache::LRUCache(int maxSize){
 }
 
 void LRUCache::insertKeyValue(string key,int value){
-   // Your implementation here
+
+	//the key exists -> replace
+	if(m.find(key) != m.end()){
+		//update the value
+		m[key]->value = value;
+
+		//it is now the most RU.
+		//shift all keys
+		
+		//store the iterator corresponding to key, points to Node
+		list<Node>::iterator nodeIt = m[key];
+		//copy the Node at the position indicated by NodeIt
+		Node moveNode = *nodeIt;
+
+		//delete the Node at key from node list
+		l.erase(nodeIt);
+
+		//put the copied node at the end.
+		l.push_back(moveNode);
+
+		//update the map, the key is now the last node.
+		m[key] = prev(l.end());
+		return;
+	}
+
+	//the key doesn't exist
+	else{
+		//the cache is full
+		if(l.size() >= maxSize){
+			//remove the LEAST (front) used key from map
+			m.erase(l.front().key);
+			//remove the corresponding node from list
+			l.pop_front();
+		}
+		//proceed
+
+		//insert new node at end
+		l.push_back(Node(key,value));
+		//update the iterator with the key, with the last node.
+		m[key] = prev(l.end());
+	
+	}
+
 }
 
 int* LRUCache::getValue(string key){
     // Your implementation here
-    return nullptr;
+
+	//doesn't exist
+	if(m.find(key) == m.end()){
+		return nullptr;
+	}else{
+		//key exists, so store a pointer to the Node
+		list<Node>::iterator it = m[key]; 
+		//dereference the node pointer's value
+		int* nodeValue = &it->value;
+		
+		//upate most recently used key.
+		//this algorithm is defined and used in insertion.
+		list<Node>::iterator nodeIt = m[key];
+		Node moveNode = *nodeIt;
+		l.erase(nodeIt);
+		l.push_back(moveNode);
+		m[key] = prev(l.end());
+
+		//return dereferenced value.
+		return nodeValue;
+	}
 
 }
 
 string LRUCache::mostRecentKey(){
-    // Your implementation here
-	return "";
+	//empty
+	if(m.empty()){
+		return "";
+	}else{
+		//not empty, store last Node
+		list<Node>::iterator lastNode = prev(l.end());
+		//store last node's key
+		string recentKey = lastNode->key;
+
+		//return key
+		return recentKey;
+	}
 }
 
 #endif
